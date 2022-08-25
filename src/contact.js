4d1b6c51dbe9200db56fe9f1f180f7db5324004a
Scott <scott.ti312@gmail.com>
2022-08-24 18:31:58 -0700

    Add contact page

--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,19 @@
 export default function contactContent() {
-  let content = document.createElement("div");
-  content.id = ("content");
-  content.innerText = "contact test";
+  let content = document.createElement('div');
+  content.id = ('content');
+  let container = document.createElement('div');
+  let intro = document.createElement('div');
+  let phone = document.createElement('div');
+  let email = document.createElement('div');
+  let address = document.createElement('div');
+  container.id = 'contact';
+
+  intro.innerText = 'Questions? Contact us.';
+  intro.id = 'intro';
+  phone.innerText = '+1 (123) 456-7890';
+  email.innerText = 'restaurant@example.com';
+  address.innerText = '12345 Molly Ln, Seattle, 67890';
+  container.append(intro, phone, email, address);
+  content.append(container);
   return content;
 }
